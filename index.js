22b81805f6e786f9d8ab3344db01afd2ad0ca538
Yurii Cherkasov <strategarius@protonmail.com>
2025-12-21 23:12:28 +0700

    Restore index

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Happy developing ✨')
+// index.js
+(() => {
+    const quotes = [
+        "Verba volant scripta manent",
+        "Fortes fortuna adiuvat",
+        "Festina lente",
+        "Audentes fortuna iuvat",
+        "Carpe diem",
+        "Amor vincit omnia",
+        "Alea iacta est",
+        "Mens sana in corpore sano",
+    ];
+
+    const el = document.querySelector("p.latin-quote");
+    if (!el) return;
+
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    el.textContent = randomQuote;
+})();
