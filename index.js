57f77b02b9d16b2d9376b9378ed701abacfbbeab
Yurii Cherkasov <strategarius@protonmail.com>
2025-12-21 23:28:53 +0700

    Introduce quotes

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 // index.js
 (() => {
     const quotes = [
-        "Verba volant scripta manent",
-        "Fortes fortuna adiuvat",
-        "Festina lente",
-        "Audentes fortuna iuvat",
-        "Carpe diem",
-        "Amor vincit omnia",
-        "Alea iacta est",
-        "Mens sana in corpore sano",
+        "Verba volant, Scripta manent",         // Spoken words fly away, written ones remain
+        "Vasa vana plurimum sonant",            // Empty pots make the most noise
+        "In absentia lucis, Tenebrae vincunt",  // In absence of light, darkness prevails
+        "Carpe diem",                           // Live today
+        "Amor vincit omnia",                    // Love conquers all
+        "Alea iacta est",                       // The die is cast
+        "Dum inter homines sumus, Colamus humanitatem",             // Among humans, be humane
+        "Disce quasi semper victurus vive quasi cras moriturus",    // Learn as if you live forever, live as if you die tomorrow
+        "Ut avertam oculos meos ad intendum",   // I close my eyes to see
+        "Qui tacet consentire",                 // Whoever silent consents to anything
+        "Parva leves capiunt animas",           // Small things occupy light minds
+        "Barba tenus sapientes",                // His wisdom as long as his beard
+        "Doscendo discimus",                    // By teaching we learn
+        "Bellum se ipsum alet",                 // War feeds itself
+        "De hoc multi multa, Omnes aliquid, Nemo satis" // They said many things, all something, no one enough
     ];
 
     const el = document.querySelector("p.latin-quote");
-    if (!el) return;
-
-    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-    el.textContent = randomQuote;
+    if (!el) {
+        return;
+    }
+    el.textContent = quotes[Math.floor(Math.random() * quotes.length)];
 })();
