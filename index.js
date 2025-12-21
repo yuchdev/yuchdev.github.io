a08a11df05323284c5da2c4ed004268188191ff9
Yurii Cherkasov <strategarius@protonmail.com>
2025-12-21 23:41:53 +0700

    Update quotes

--- index.js
+++ index.js
@@ -15,7 +15,19 @@
         "Barba tenus sapientes",                // His wisdom as long as his beard
         "Doscendo discimus",                    // By teaching we learn
         "Bellum se ipsum alet",                 // War feeds itself
-        "De hoc multi multa, Omnes aliquid, Nemo satis" // They said many things, all something, no one enough
+        "De hoc multi multa, Omnes aliquid, Nemo satis",    // They said many things, all something, no one enough
+        "Accensa domo proximi, Tua quoque periclitatur",    // When neighbor's house is on fire, your own is in danger
+        "Omnes una manet nox",                  // One night awaits everyone
+        "Igne natura renovatur integra",        // Nature reborn through fire
+        "Nunquam non paratus",                  // Never unprepared is always ready
+        "Non progredi est regredi",             // Not going forward is going backward
+        "Factum fieri infectum non potest",     // The deed can't be undone
+        "Eo ipso",                              // By this act or fact
+        "Nullus agenti dies longus est",        // No day is long for hardworking
+        "Dixi",                                 // I have spoken
+        "Caesar non supra grammaticos",         // Caesar is not above grammar
+        "Corruptissima republica plurimae leges",    // The more numerous the laws, the more corrupt the state
+        "Omnium Rerum Principia Parva Sunt"     // All the principles are simple and constant
     ];
 
     const el = document.querySelector("p.latin-quote");
